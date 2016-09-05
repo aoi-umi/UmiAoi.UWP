b4fc40c3629b756586b91e820d090b048a023e4e
aoi-umi <1641622365@qq.com>
2016-09-05 16:09:12 +0800

    +MultiPageRichTextBox

--- UmiAoi.UWP/Controls/MultiPageRichTextBox.xaml.cs
+++ UmiAoi.UWP/Controls/MultiPageRichTextBox.xaml.cs
@@ -11,7 +11,10 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Documents;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -22,6 +25,70 @@ namespace UmiAoi.UWP.Controls
         public MultiPageRichTextBox()
         {
             this.InitializeComponent();
+            AddHandler(UIElement.PointerReleasedEvent, new PointerEventHandler(PointerReleased), true);
+            InitStoryBoard();
+            var p1 = new Paragraph();
+            p1.Inlines.Add(new Run() { Text = "aaaaaaaaaaaaaaaaaaaaa1" });
+            var p2 = new Paragraph();
+            p2.Inlines.Add(new Run() { Text = "aaaaaaaaaaaaaaaaaaaaa2" });
+            var p3 = new Paragraph();
+            p3.Inlines.Add(new Run() { Text = "aaaaaaaaaaaaaaaaaaaaa3" });
+            rtb.Blocks.Add(p1);
+            rtb.Blocks.Add(p2);
+            rtb.Blocks.Add(p3);
+        }
+
+        private readonly Storyboard storyboard = new Storyboard();
+        private readonly DoubleAnimation xAnim = new DoubleAnimation();
+        private void InitStoryBoard()
+        {
+            storyboard.Completed += Storyboard_Completed;
+            var element = img;
+            var ct = new CompositeTransform();
+            element.RenderTransform = ct;
+            xAnim.EasingFunction = new ExponentialEase { Exponent = 4 };
+            xAnim.Duration = new Duration(TimeSpan.FromMilliseconds(1000));
+            Storyboard.SetTarget(xAnim, element.RenderTransform);
+            Storyboard.SetTargetProperty(xAnim, nameof(CompositeTransform.TranslateX));
+            storyboard.Children.Add(xAnim);
+        }
+
+        private void Storyboard_Completed(object sender, object e)
+        {
+            img.Visibility = Visibility.Collapsed;
+        }
+
+        private void PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            var point = e.GetCurrentPoint((UIElement)sender);
+            var pos = point.Position;
+            if (pos.X > ActualWidth / 2)
+            {
+                Animation(sv.VerticalOffset + sv.ActualHeight, true);
+            }
+            else
+            {
+                Animation(sv.VerticalOffset - sv.ActualHeight, false);
+            }
+        }
+
+        private async void Animation(double offset, bool toLeft)
+        {
+            img.Visibility = Visibility.Visible;
+            RenderTargetBitmap bitmap = new RenderTargetBitmap();
+            await bitmap.RenderAsync(sv);
+            img.Source = bitmap;
+            sv.ScrollToVerticalOffset(offset);
+            xAnim.From = 0;
+            xAnim.To = toLeft ? -ActualWidth : ActualWidth;
+            storyboard.Stop();
+            storyboard.Begin();
+        }
+
+        ~MultiPageRichTextBox()
+        {
+            storyboard.Completed -= Storyboard_Completed;
+            RemoveHandler(UIElement.PointerReleasedEvent, new PointerEventHandler(PointerReleased));
         }
     }
 }
