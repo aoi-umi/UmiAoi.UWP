db7533d5d57f0abb83e8eea83b56db2451553125
aoi-umi <1641622365@qq.com>
2016-09-07 18:08:37 +0800

    +Helper

---
+++ UmiAoi.UWP/Helper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace UmiAoi.UWP
+{
+    public static class Helper
+    {
+        public static void BindingHelper(BindingModel bindingModel)
+        {
+            if (bindingModel.BindingElement == null) throw new NullReferenceException(nameof(bindingModel.BindingElement));
+            var binding = new Binding();
+            if (bindingModel.Source != null) binding.Source = bindingModel.Source;
+            if (bindingModel.Path != null) binding.Path = new PropertyPath(bindingModel.Path);
+            if (bindingModel.ElementName != null) binding.ElementName = bindingModel.ElementName;
+            binding.Mode = bindingModel.BindingMode;
+            bindingModel.BindingElement.SetBinding(bindingModel.Property, binding);
+        }
+
+        public static async void ShowMessage(string message, string title = null)
+        {
+            await new MessageDialog(message, title).ShowAsync();
+        }
+    }
+
+    public class BindingModel
+    {
+        public FrameworkElement BindingElement { get; set; }
+        public DependencyProperty Property { get; set; }
+        public Object Source { get; set; }
+        public string Path { get; set; }
+        public BindingMode BindingMode { get; set; }
+        public String ElementName { get; set; }
+    }
+}
