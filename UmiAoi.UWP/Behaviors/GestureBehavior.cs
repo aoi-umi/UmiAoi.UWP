85f1ceea5100370bd64a8f48e58f5e9e2f68fab4
aoi-umi <1641622365@qq.com>
2016-10-12 18:04:52 +0800

    双击放大缩小增加过渡动画

--- UmiAoi.UWP/Behaviors/GestureBehavior.cs
+++ UmiAoi.UWP/Behaviors/GestureBehavior.cs
@@ -1,14 +1,7 @@
 ﻿using Microsoft.Xaml.Interactivity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 
 namespace UmiAoi.UWP.Behaviors
@@ -16,27 +9,35 @@ namespace UmiAoi.UWP.Behaviors
     public class GestureBehavior : DependencyObject, IBehavior
     {
         public DependencyObject AssociatedObject => _associatedObject;
-        private DependencyObject _associatedObject { get; set; }     
+        private DependencyObject _associatedObject { get; set; }
         private FrameworkElement _element { get; set; }
-
-        //private bool IsBusy = false;
-        //private Storyboard _storyboard { get; set; }
-        //private DoubleAnimation _XAnim { get; set; }
-        //private DoubleAnimation _YAnim { get; set; }
+        
+        private Storyboard _storyboard { get; set; }
+        private DoubleAnimation _xAnim { get; set; }
+        private DoubleAnimation _yAnim { get; set; }
         public void Attach(DependencyObject associatedObject)
         {
             _associatedObject = associatedObject;
             _element = _associatedObject as FrameworkElement;
             if (_element == null) return;
 
-            //_XAnim = new DoubleAnimation();
-            //_XAnim.EasingFunction = new ExponentialEase { Exponent = 4 };
-            //_XAnim.Duration = new Duration(TimeSpan.FromMilliseconds(100));
-            //Storyboard.SetTarget(_XAnim, _element);
-            //Storyboard.SetTargetProperty(_XAnim, nameof(FrameworkElement.Width));
-
-            //_storyboard = new Storyboard();
-            //_storyboard.Children.Add(_XAnim);
+            _xAnim = new DoubleAnimation()
+            {
+                EnableDependentAnimation = true,
+                EasingFunction = new ExponentialEase { Exponent = 4 },
+                Duration = new Duration(TimeSpan.FromMilliseconds(500))
+            };
+            _yAnim = new DoubleAnimation()
+            {
+                EnableDependentAnimation = true,
+                EasingFunction = new ExponentialEase { Exponent = 4 },
+                Duration = new Duration(TimeSpan.FromMilliseconds(500))
+            };
+            Storyboard.SetTarget(_xAnim, _element);
+            Storyboard.SetTarget(_yAnim, _element);
+            Storyboard.SetTargetProperty(_xAnim, nameof(FrameworkElement.Width));
+            Storyboard.SetTargetProperty(_yAnim, nameof(FrameworkElement.Height));
+            _storyboard = new Storyboard();
 
             _element.ManipulationMode = ManipulationModes.System | ManipulationModes.Scale;
             _element.DoubleTapped += Element_DoubleTapped;
@@ -44,24 +45,22 @@ namespace UmiAoi.UWP.Behaviors
             _element.PointerWheelChanged += _element_PointerWheelChanged;
         }
 
-
-
         private void _element_PointerWheelChanged(object sender, PointerRoutedEventArgs e)
         {
+            var delta = (double)e.GetCurrentPoint(_element).Properties.MouseWheelDelta / 1000;
+            if (_element.ActualWidth < 50 && (1 + delta) <= 1) return;
             _element.Height = double.NaN;
             _element.Width = double.NaN;
-            var delta = (double)e.GetCurrentPoint(_element).Properties.MouseWheelDelta / 1000;
-            if (_element.ActualWidth < 50 && (1 + delta) <= 0) return;
             _element.Width = _element.ActualWidth * (1 + delta);
             e.Handled = true;
         }
 
         private void _element_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
         {
-            _element.Height = double.NaN;
-            _element.Width = double.NaN;
             var delta = e.Delta.Scale;
             if (_element.ActualWidth < 50 && (delta) <= 0) return;
+            _element.Height = double.NaN;
+            _element.Width = double.NaN;
             _element.Width = _element.ActualWidth * (delta);
             e.Handled = true;
         }
@@ -73,24 +72,35 @@ namespace UmiAoi.UWP.Behaviors
             {
                 if (_element.Width != parent.ActualWidth)
                 {
-                    _element.Width = parent.ActualWidth;
+                    StartStoryboard(_element.ActualWidth, parent.ActualWidth, true);
                     _element.Height = double.NaN;
                 }
                 else
                 {
-                    _element.Height = parent.ActualHeight;
+                    StartStoryboard(_element.ActualHeight, parent.ActualHeight, false);
                     _element.Width = double.NaN;
                 }
             }
         }
 
-        //private void StartStoryboard(double from, double to)
-        //{
-        //    _storyboard.Stop();
-        //    _XAnim.From = from;
-        //    _XAnim.To = to;
-        //    _storyboard.Begin();
-        //}
+        private void StartStoryboard(double from, double to, bool isWidth)
+        {
+            _storyboard.Stop();
+            _storyboard.Children.Clear();
+            if (isWidth)
+            {
+                _xAnim.From = from;
+                _xAnim.To = to;
+                _storyboard.Children.Add(_xAnim);
+            }
+            else
+            {
+                _yAnim.From = from;
+                _yAnim.To = to;
+                _storyboard.Children.Add(_yAnim);
+            }
+            _storyboard.Begin();
+        }
 
         public void Detach()
         {
