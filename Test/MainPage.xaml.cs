569decdf6734a29a1fcef82c0c8e322390b5009f
aoi-umi <1641622365@qq.com>
2016-10-17 18:20:41 +0800

    CircleMenu, sencond edit

--- Test/MainPage.xaml.cs
+++ Test/MainPage.xaml.cs
@@ -5,16 +5,12 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI;
-using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -29,88 +25,6 @@ namespace Test
         public MainPage()
         {
             this.InitializeComponent();
-            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
-            {
-                StatusBar statusBar = StatusBar.GetForCurrentView();
-                statusBar.ForegroundColor = Colors.White;
-                statusBar.BackgroundColor = Colors.Black;
-                statusBar.BackgroundOpacity = 1;
-            }
-            InitStoryBoard();
-        }
-
-        ApplicationView view = ApplicationView.GetForCurrentView();
-        private void EnterFullScreen_Click(object sender, RoutedEventArgs e)
-        {
-            bool isInFullScreenMode = view.IsFullScreenMode;
-            switch (cb.SelectedIndex)
-            {
-                case 0:
-                    view.TryEnterFullScreenMode();
-                    break;
-                case 1:
-                    ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.Auto;
-                    break;
-                case 2:
-                    ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
-                    break;
-                case 3:
-                    ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
-                    break;
-                case 4:
-                    // 在全屏状态下，是否显示系统 UI，比如标题栏和任务栏
-                    view.FullScreenSystemOverlayMode = FullScreenSystemOverlayMode.Minimal;
-                    view.ShowStandardSystemOverlays();
-                    break;
-                case 5:
-                    // 在全屏状态下，是否显示系统 UI，比如标题栏和任务栏
-                    view.FullScreenSystemOverlayMode = FullScreenSystemOverlayMode.Standard;
-                    view.ShowStandardSystemOverlays();
-                    break;
-            }
-        }
-
-        private void ExitFullScreen_Click(object sender, RoutedEventArgs e)
-        {
-            view.ExitFullScreenMode();
-        }
-
-        private void NextPage_Click(object sender, RoutedEventArgs e)
-        {
-            SetRect(sv.VerticalOffset + sv.ActualHeight, true);
-        }
-
-        private void PrevPage_Click(object sender, RoutedEventArgs e)
-        {
-            SetRect(sv.VerticalOffset - sv.ActualHeight, false);
-        }
-
-        private readonly Storyboard _storyboard = new Storyboard();
-        private readonly DoubleAnimation _XAnim = new DoubleAnimation();
-        private async void SetRect(double offset, bool next)
-        {
-            RenderTargetBitmap bitmap = new RenderTargetBitmap();
-            await bitmap.RenderAsync(sv);
-            rect.Source = bitmap;
-            sv.ScrollToVerticalOffset(offset);
-            _XAnim.From = 0;
-            _XAnim.To = next ? -sv.ActualWidth : sv.ActualWidth;
-            _storyboard.Begin();
-        }
-
-        private async void InitStoryBoard()
-        {
-            RenderTargetBitmap bitmap = new RenderTargetBitmap();
-            await bitmap.RenderAsync(sv);
-            rect.Source = bitmap;
-            var element = rectGrid;
-            var ct = new CompositeTransform();
-            element.RenderTransform = ct;
-            _XAnim.EasingFunction = new ExponentialEase { Exponent = 4 };
-            _XAnim.Duration = new Duration(TimeSpan.FromMilliseconds(1000));
-            Storyboard.SetTarget(_XAnim, element.RenderTransform);
-            Storyboard.SetTargetProperty(_XAnim, nameof(CompositeTransform.TranslateX));
-            _storyboard.Children.Add(_XAnim);
         }
     }
 }
--- UmiAoi.UWP/Controls/CircleMenu.cs
+++ UmiAoi.UWP/Controls/CircleMenu.cs
@@ -36,76 +36,69 @@ namespace UmiAoi.UWP.Controls
             (d as CircleMenu).UpdateUI();
         }
 
+        public double ThetaAngle
+        {
+            get { return (double)GetValue(ThetaAngleProperty); }
+            set { SetValue(ThetaAngleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ThetaRadians.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ThetaAngleProperty =
+            DependencyProperty.Register(nameof(ThetaAngle), typeof(double), typeof(CircleMenu), new PropertyMetadata(double.NaN, new PropertyChangedCallback(ThetaAngleChanged)));
+
+        private static void ThetaAngleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as CircleMenu).UpdateUI();
+        }
+
         private Canvas canvas { get; set; }
         protected override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-            foreach (var item in Items)
-            {
-                var element = item as FrameworkElement;
-                if (element != null)
-                {
-                    var bindingModel = new BindingModel()
-                    {
-                        Source = this,
-                        Path = nameof(Width),
-                        BindingElement = element,
-                        Property = FrameworkElement.WidthProperty,
-                        BindingMode = BindingMode.OneWay
-                    };
-                    Helper.BindingHelper(bindingModel);
-                    bindingModel.Path = nameof(Height);
-                    bindingModel.Property = FrameworkElement.HeightProperty;
-                    Helper.BindingHelper(bindingModel);
-                }
-            }
             canvas = GetTemplateChild(CanvasName) as Canvas;
             UpdateUI();
-
         }
                 
         protected override void OnItemsChanged(object e)
         {
-            base.OnItemsChanged(e);
-            var element = e as FrameworkElement;
-            if (element != null)
-            {
-                var bindingModel = new BindingModel()
-                {
-                    Source = this,
-                    Path = nameof(Width),
-                    BindingElement = element,
-                    Property = FrameworkElement.WidthProperty,
-                    BindingMode = BindingMode.OneWay
-                };
-                Helper.BindingHelper(bindingModel);
-                bindingModel.Path = nameof(Height);
-                bindingModel.Property = FrameworkElement.HeightProperty;
-                Helper.BindingHelper(bindingModel);
-            }
+            base.OnItemsChanged(e);            
             UpdateUI();
         }
-
+        
         private void UpdateUI()
         {
             if (canvas != null)
             {
                 canvas.Children.Clear();
                 double theta = 0;
-                double thetaRadians = 270;
+                double thetaRadians = 0;
                 int count = Items.Count;
                 foreach (var item in Items)
                 {
                     var element = item as FrameworkElement;
                     if (element != null)
                     {
-                        canvas.Children.Add(element);
+                        var bindingModel = new BindingModel()
+                        {
+                            Source = this,
+                            Path = nameof(Width),
+                            BindingElement = element,
+                            Property = FrameworkElement.WidthProperty,
+                            BindingMode = BindingMode.OneWay
+                        };
+                        Helper.BindingHelper(bindingModel);
+                        bindingModel.Path = nameof(Height);
+                        bindingModel.Property = FrameworkElement.HeightProperty;
+                        Helper.BindingHelper(bindingModel);
+
+                        if (!canvas.Children.Contains(element)) canvas.Children.Add(element);
                         //var diagonaphal = Math.Sqrt(Math.Pow(element.ActualWidth, 2) + Math.Pow(element.ActualHeight, 2)) / 2;
-                        var x = (double)(CircleRadius * Math.Cos(thetaRadians) - element.Width / 2);
-                        var y = (double)(CircleRadius * Math.Sin(thetaRadians) - element.Height / 2);
+                        var x = (double)(CircleRadius * Math.Sin(thetaRadians) - element.Width / 2);
+                        var y = (double)(-CircleRadius * Math.Cos(thetaRadians) - element.Height / 2);
                         Canvas.SetLeft(element, x);
                         Canvas.SetTop(element, y);
-                        theta += 360 / count;
+                        if (double.IsNaN(ThetaAngle)) theta += 360F / count;
+                        else theta += ThetaAngle;
                         thetaRadians = theta * Math.PI / 180F;
                     }
                 }
